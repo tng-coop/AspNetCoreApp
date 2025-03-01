d95aceae01cc1f6260a4b1163e7d4c8e4193123b
Yasuaki Kudo <yasu@tng.coop>
2025-03-02 00:18:20 +0900

    a

---
+++ Pages/Members.cshtml.cs
@@ -0,0 +1,23 @@
+using AspNetCoreApp.Data;
+using AspNetCoreApp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreApp.Pages;
+
+public class MembersModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public MembersModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IList<Member> Members { get; set; } = [];
+
+    public async Task OnGetAsync()
+    {
+        Members = await _context.Members.ToListAsync();
+    }
+}
