74f684449accdef0e059ce8d84bb76e595e4a6b7
Yasuaki Kudo <yasu@tng.coop>
2025-03-04 00:50:43 +0900

    a

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -30,5 +30,22 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
             new Member { Id = 13, FirstName = "Matthias", LastName = "", Email = "matthias@example.com" },
             new Member { Id = 14, FirstName = "Paul", LastName = "", Email = "paul@example.com" }
         );
+
+        modelBuilder.Entity<IdentityUser>().HasData(
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "simon.peter@example.com", NormalizedUserName = "SIMON.PETER@EXAMPLE.COM", Email = "simon.peter@example.com", NormalizedEmail = "SIMON.PETER@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "andrew@example.com", NormalizedUserName = "ANDREW@EXAMPLE.COM", Email = "andrew@example.com", NormalizedEmail = "ANDREW@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "james.zebedee@example.com", NormalizedUserName = "JAMES.ZEBEDEE@EXAMPLE.COM", Email = "james.zebedee@example.com", NormalizedEmail = "JAMES.ZEBEDEE@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "john.zebedee@example.com", NormalizedUserName = "JOHN.ZEBEDEE@EXAMPLE.COM", Email = "john.zebedee@example.com", NormalizedEmail = "JOHN.ZEBEDEE@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "philip@example.com", NormalizedUserName = "PHILIP@EXAMPLE.COM", Email = "philip@example.com", NormalizedEmail = "PHILIP@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "bartholomew@example.com", NormalizedUserName = "BARTHOLOMEW@EXAMPLE.COM", Email = "bartholomew@example.com", NormalizedEmail = "BARTHOLOMEW@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "thomas@example.com", NormalizedUserName = "THOMAS@EXAMPLE.COM", Email = "thomas@example.com", NormalizedEmail = "THOMAS@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "matthew.levi@example.com", NormalizedUserName = "MATTHEW.LEVI@EXAMPLE.COM", Email = "matthew.levi@example.com", NormalizedEmail = "MATTHEW.LEVI@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "james.alphaeus@example.com", NormalizedUserName = "JAMES.ALPHAEUS@EXAMPLE.COM", Email = "james.alphaeus@example.com", NormalizedEmail = "JAMES.ALPHAEUS@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "thaddaeus@example.com", NormalizedUserName = "THADDAEUS@EXAMPLE.COM", Email = "thaddaeus@example.com", NormalizedEmail = "THADDAEUS@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "simon.zealot@example.com", NormalizedUserName = "SIMON.ZEALOT@EXAMPLE.COM", Email = "simon.zealot@example.com", NormalizedEmail = "SIMON.ZEALOT@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "judas.iscariot@example.com", NormalizedUserName = "JUDAS.ISCARIOT@EXAMPLE.COM", Email = "judas.iscariot@example.com", NormalizedEmail = "JUDAS.ISCARIOT@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "matthias@example.com", NormalizedUserName = "MATTHIAS@EXAMPLE.COM", Email = "matthias@example.com", NormalizedEmail = "MATTHIAS@EXAMPLE.COM", EmailConfirmed = true },
+            new IdentityUser { Id = Guid.NewGuid().ToString(), UserName = "paul@example.com", NormalizedUserName = "PAUL@EXAMPLE.COM", Email = "paul@example.com", NormalizedEmail = "PAUL@EXAMPLE.COM", EmailConfirmed = true }
+        );
     }
 }
