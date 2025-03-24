0691fdf31d7e211ffccecd7c8934cd18042d9361
Yasuaki Kudo <yasu@tng.coop>
2025-03-24 15:25:57 +0900

    c

--- show-files.sh
+++ show-files.sh
@@ -1,9 +1,26 @@
-FILES=$(find . -type f| egrep -vi '(./git|./published|node_module|logs|docs|./bin|./out|/obj|/.git|tests|asset|migratio|\.sh$|launchSett|\.txt$|\.md$|\.css$|\.mjs$|\.env$|localhost|Dockerfile|package-lock|package.json|\.ps1$)')
+#!/bin/bash
+
+name_only=false
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -n|--name-only) name_only=true ;;
+    *) echo "Unknown option: $1"; exit 1 ;;
+  esac
+  shift
+done
+
+FILES=$(find . -type f | egrep -vi '(./git|./published|node_module|logs|docs|./bin|./out|/obj|/.git|tests|asset|migratio|\.sh$|launchSett|\.txt$|\.md$|\.css$|\.mjs$|\.env$|localhost|Dockerfile|package-lock|package.json|\.ps1$)')
+
 for f in $FILES
 do
-  echo $f
-  echo "----------------"
-
-  cat $f
-  echo "----------------"
+  if [ "$name_only" = true ]; then
+    echo $f
+  else
+    echo $f
+    echo "----------------"
+    cat $f
+    echo "----------------"
+  fi
 done
