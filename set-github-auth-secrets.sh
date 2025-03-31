fc1b6497e436cc2c5f3f31e12ea2b83a2d492b04
Yasuaki Kudo <yasu@tng.coop>
2025-03-31 16:22:11 +0900

    c

--- set-github-auth-secrets.sh
+++ set-github-auth-secrets.sh
@@ -6,6 +6,8 @@ required_vars=(
   Authentication__GitHub__ClientSecret
   Authentication__LINE__ClientId 
   Authentication__LINE__ClientSecret
+  Authentication__Google__ClientId 
+  Authentication__Google__ClientSecret
   )
 
 for var in "${required_vars[@]}"; do
@@ -20,5 +22,7 @@ gh secret set AUTH_GITHUB_CLIENT_ID --body "$Authentication__GitHub__ClientId"
 gh secret set AUTH_GITHUB_CLIENT_SECRET --body "$Authentication__GitHub__ClientSecret"
 gh secret set AUTH_LINE_CLIENT_ID --body "$Authentication__LINE__ClientId"
 gh secret set AUTH_LINE_CLIENT_SECRET --body "$Authentication__LINE__ClientSecret"
+gh secret set AUTH_GOOGLE_CLIENT_ID --body "$Authentication__Google__ClientId"
+gh secret set AUTH_GOOGLE_CLIENT_SECRET --body "$Authentication__Google__ClientSecret"
 
 echo "✅ GitHub Actions secrets set successfully!"
