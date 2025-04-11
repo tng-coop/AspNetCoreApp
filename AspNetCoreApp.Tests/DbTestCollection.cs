71f4f9ee0da9da41ef0047ebb077884ab2a3014a
Yasuaki Kudo <yasu@tng.coop>
2025-04-11 13:11:49 +0900

    f

--- AspNetCoreApp.Tests/DbTestCollection.cs
+++
@@ -1,5 +0,0 @@
-[CollectionDefinition("DbTestCollection")]
-public class DbTestCollection : ICollectionFixture<DbTestFixture>
-{
-    // This class is just a marker for the collection fixture
-}
--- AspNetCoreApp/wwwroot/js/qr.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-    const qrCodeData = document.getElementById("qrCodeData").value;
-    new QRCode(document.getElementById("qrCode"), {
-      text: qrCodeData,
-      width: 150,
-      height: 150
-    });
-  });
-  
