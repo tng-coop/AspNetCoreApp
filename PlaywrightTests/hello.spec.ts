280769fe1b37a3693f29d7b8f01eaad3a95088b3
Yasuaki Kudo <yasu@tng.coop>
2025-05-17 21:19:03 +0900

    a

--- PlaywrightTests/hello.spec.ts
+++ PlaywrightTests/hello.spec.ts
@@ -4,3 +4,8 @@ test('Login exists', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByText('Login')).toHaveCount(1)
 });
+
+test('Cert exists', async ({ page }) => {
+  await page.goto('http://aspnet.lan/cert/');
+  await expect(page.getByText('aspnet.lan-ca.crt')).toHaveCount(1)
+});
