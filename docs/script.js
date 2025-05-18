3d35fa4f4ce60e4dedd625ab1b46b268bb64e1fe
Yasuaki Kudo <yasu@tng.coop>
2025-05-18 14:12:17 +0900

    a

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const contactForm = document.getElementById('contact-form');
-    contactForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert('Thank you for contacting us!');
-        contactForm.reset();
-    });
-});
