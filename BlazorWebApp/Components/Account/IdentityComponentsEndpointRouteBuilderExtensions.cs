d73d0429699371093d1884781bcf710db7704f5a
Yasuaki Kudo <yasu@tng.coop>
2025-05-24 20:03:09 +0900

    a

--- BlazorWebApp/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ BlazorWebApp/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -42,11 +42,10 @@ internal static class IdentityComponentsEndpointRouteBuilderExtensions
 
         accountGroup.MapPost("/Logout", async (
             ClaimsPrincipal user,
-            [FromServices] SignInManager<ApplicationUser> signInManager,
-            [FromForm] string returnUrl) =>
+            [FromServices] SignInManager<ApplicationUser> signInManager) =>
         {
             await signInManager.SignOutAsync();
-            return TypedResults.LocalRedirect($"~/{returnUrl}");
+            return TypedResults.LocalRedirect("~/");
         });
 
         var manageGroup = accountGroup.MapGroup("/Manage").RequireAuthorization();
