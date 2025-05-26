2ef28790e1438cf618c448fe074c9df0c5e99430
Yasuaki Kudo <yasu@tng.coop>
2025-05-26 23:26:04 +0900

    a

--- BlazorWebApp.Tests/UnitTest1.cs
+++ BlazorWebApp.Tests/UnitTest1.cs
@@ -55,38 +55,5 @@ public class CounterComponentTests : TestContext
         public byte[] Unprotect(byte[] protectedData) => protectedData;
     }
 
-    public CounterComponentTests()
-    {
-        // Localization: point to Resources folder and register services
-        Services.AddLocalization(options => options.ResourcesPath = "Resources");
-        // Ensure culture is consistent
-        CultureInfo.DefaultThreadCurrentCulture   = new CultureInfo("en-US");
-        CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 
-        // If AddLocalization doesn't wire up the generic localizer, register it:
-        Services.AddSingleton(typeof(IStringLocalizer<>), typeof(StringLocalizer<>));
-
-        // Register other dependencies
-        Services.AddScoped<ProtectedLocalStorage>(sp =>
-            new ProtectedLocalStorage(new DummyJSRuntime(), new DummyDataProtectionProvider()));
-        Services.AddSingleton<AuthenticationStateProvider>(new DummyAuthStateProvider());
-        Services.AddSingleton<IServiceScopeFactory>(sp => new DummyScopeFactory(sp));
-        Services.AddScoped<LocalizationService>();
-    }
-
-    [Fact]
-    public void CounterIncrementsWhenClicked()
-    {
-        // Act
-        var cut = RenderComponent<Counter>();
-
-        // Assert initial state is zero
-        Assert.Contains("Current count: 0", cut.Markup);
-
-        // Perform click
-        cut.Find("button").Click();
-
-        // After click, count should be one
-        Assert.Contains("Current count: 1", cut.Markup);
-    }
 }
