14aff854e87d9b690ceaa9b829c9c25730bfcd40
Yasuaki Kudo <yasu@tng.coop>
2025-05-26 12:13:42 +0900

    a

--- BlazorWebApp/Migrations/20250526031327_InitialCreate.Designer.cs
+++ BlazorWebApp/Migrations/20250526031327_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlazorWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250526031111_InitialCreate")]
+    [Migration("20250526031327_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
